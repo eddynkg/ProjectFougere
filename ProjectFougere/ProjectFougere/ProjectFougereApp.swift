4ce6188aba01d5ece0f6e23ec89230edc86222d4
Eddy <eddyelame@gmail.com>
2025-10-27 14:10:28 +0100

    First commit : création du projet

---
+++ ProjectFougere/ProjectFougere/ProjectFougereApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectFougereApp.swift
+//  ProjectFougere
+//
+//  Created by Eddy Charles on 27/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct ProjectFougereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProjectFougere/ProjectFougereTests/ProjectFougereTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectFougereTests.swift
+//  ProjectFougereTests
+//
+//  Created by Eddy Charles on 27/10/2025.
+//
+
+import Testing
+@testable import ProjectFougere
+
+struct ProjectFougereTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
